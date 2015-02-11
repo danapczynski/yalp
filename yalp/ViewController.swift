bcd4b19ffcb8ca5ad341587daf088252c742492d
Dan Apczynski <dapczynski@zendesk.com>
2015-02-10 22:42:38 -0800

    Initial Commit

---
+++ yalp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yalp
+//
+//  Created by Daniel Apczynski on 2/10/15.
+//  Copyright (c) 2015 Dan Apczynski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
